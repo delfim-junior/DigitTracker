cf8f525fef9041adeadb05e73f245ec483827ae9
delfim-junior <delfim.uu.junior@gmail.com>
2020-08-26 12:48:57 +0200

    password-recovery layout

--- tracker/src/pages/PasswordRecovery/index.js
+++ tracker/src/pages/PasswordRecovery/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
+import { OutlinedInput } from '@material-ui/core';
+
+import logo from '../../assets/images/icon.JPG'
 
 import './styles.css';
 
+
 function PasswordRecovery() {
-  return(
+  const [email, setEmail] = useState()
+
+  function handleRecovery(event) {
+    event.preventDefault()
+    alert('yeah')
+  }
+
+  return (
     <div className="container">
-      <h5>ICON</h5>
-      <h5>Oops, you forgot your password! Don't worry, access recovery is simple</h5>
+      <header className='forgot-header'>
+        <img src={logo} alt="logo" width="120px" height="120px" />
+        <h5>Oops, you forgot your password! Don't worry, access recovery is simple&#128522;</h5>
+      </header>
+
+      <form onSubmit={handleRecovery}>
+        <OutlinedInput className='forgot-input' placeholder="enter your email" InputLabel="enter your email"/>
+        
+        <footer>
+          <button type="submit">
+            Recover Access
+          </button>
+        </footer>
+      </form>
     </div>
   );
 }
