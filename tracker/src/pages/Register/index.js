1e7ac0186ca8d60a5c87ee9a18eea27e89ec6955
delfim-junior <delfim.uu.junior@gmail.com>
2020-08-26 19:26:08 +0200

    userTracker page

--- tracker/src/pages/Register/index.js
+++ tracker/src/pages/Register/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Identify from './Identify';
+import UserTracker from './UserTracker';
 
 // import { Container } from './styles';
 
 function Register() {
   return (
-    <h1>Registration</h1>
+    <Switch>
+      <Route exact path='/register' component={Identify} />
+      <Route exact path='/register/user' component={UserTracker} />
+    </Switch>
   );
 }
 
