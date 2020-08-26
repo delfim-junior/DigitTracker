f54951f07e87bceaa2c671aa9fe1a4582007160e
delfim-junior <delfim.uu.junior@gmail.com>
2020-08-26 23:57:19 +0200

    styling: Medical Professional Form

--- tracker/src/pages/Register/index.js
+++ tracker/src/pages/Register/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Identify from './Identify';
 import UserTracker from './UserTracker';
+import MedicalProfessional from './MedicalProfessional';
 
 // import { Container } from './styles';
 
@@ -9,7 +10,8 @@ function Register() {
   return (
     <Switch>
       <Route exact path='/register' component={Identify} />
-      <Route exact path='/register/user' component={UserTracker} />
+      <Route path='/register/user' component={UserTracker} />
+      <Route path='/register/professional' component={MedicalProfessional} />
     </Switch>
   );
 }
