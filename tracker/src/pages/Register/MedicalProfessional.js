928e7748c3bf5bc27c984813f936766b00f9f0be
delfim-junior <delfim.uu.junior@gmail.com>
2020-08-27 02:20:48 +0200

    form validation and toasts alert

--- tracker/src/pages/Register/MedicalProfessional.js
+++ tracker/src/pages/Register/MedicalProfessional.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiUserPlus, FiArrowLeft } from 'react-icons/fi'
+import { useHistory } from 'react-router-dom';
+import validator from 'validator'
+
 import { OutlinedInput } from '@material-ui/core';
 import { Button, Select, TextField, MenuItem, InputLabel } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import './styles.css';
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
 import Loading from '../Loading';
 
@@ -25,20 +29,88 @@ function MedicalProfessional() {
     const history = useHistory()
 
     const [successMessage, setSuccessMessage] = useState(false)
+    const [formInput, setFormInput] = useState({
+        firstName: '',
+        middleName: '',
+        lastName: '',
+        birthday: '',
+        email: '',
+        confirmEmail: '',
+        password: '',
+        confirmPassword: '',
+        city: '',
+        nationalId: '',
+        employeeId: '',
+        medicalDescription: '',
+        hospital: '',
+        role: ''
+
+    })
+
+
     function handleGoBack() {
         history.push('/')
     }
 
-    function handleRegister() {
-        setSuccessMessage(true)
-        setTimeout(() => {
-            history.push('/')
-        }, 3000)
+    function handleFormInput(event) {
+        const { name, value } = event.target
+        setFormInput({ ...formInput, [name]: value })
+    }
+
+    function handleRegister(event) {
+        event.preventDefault()
+        //data ready to use in the backend
+        const {
+            firstName,
+            middleName,
+            lastName,
+            gender,
+            birthday,
+            email,
+            confirmEmail,
+            password,
+            confirmPassword,
+            city,
+            nationalId,
+            employeeId,
+            medicalDescription,
+            hospital,
+        } = formInput
+
+        const isEmail = validator.isEmail(email)
+        if (isEmail) {
+            if (email === confirmEmail) {
+                if (password.length >= 4) {
+                    if (password === confirmPassword) {
+                        //Backend...
+                        setSuccessMessage(true)
+                        setTimeout(() => {
+                            history.push('/')
+                        }, 3000)
+                    }
+                    else {
+                        toast.error('Not corresponding Passwords. Please, make sure that the entered passwords are equal')
+                    }
+                }
+                else {
+                    toast.warn('Passoword must have the minimum of 4 characters')
+                }
+            }
+            else {
+                toast.error('Not corresponding Emails. Please, verify')
+            }
+
+        }
+        else {
+            toast.error('The entered Email is not valid')
+        }
+
     }
 
     return (
         <>
-            <form className="professional-container">
+            <ToastContainer />
+            <form onSubmit={handleRegister} className="professional-container">
                 <header>
                     <FiArrowLeft onClick={handleGoBack} size={30} color="#27AE60" />
                     <h6 onClick={handleGoBack}>Home</h6>
@@ -52,23 +124,26 @@ function MedicalProfessional() {
                         className='firsname'
                         placeholder="First Name"
                         type='text'
-                        name="firtname"
+                        name="firstName"
                         required
+                        onChange={handleFormInput}
                     />
                     <OutlinedInput
                         className='middlename'
                         placeholder="Middle Name"
                         type='text'
-                        name="middlename"
+                        name="middleName"
                         required
+                        onChange={handleFormInput}
                     />
                 </div>
                 <OutlinedInput
                     className=''
                     placeholder="Last Name"
                     type='text'
-                    name="lastname"
+                    name="lastName"
                     required
+                    onChange={handleFormInput}
                 />
                 <TextField
                     id="date"
@@ -79,39 +154,52 @@ function MedicalProfessional() {
                     InputLabelProps={{
                         shrink: true,
                     }}
+                    name="birthday"
+                    required
+                    onChange={handleFormInput}
                 />
                 <OutlinedInput
                     placeholder="Email"
                     type='text'
-                    name="Email"
+                    name="email"
                     required
+                    onChange={handleFormInput}
                 />
                 <OutlinedInput
                     placeholder="Confirm your email"
                     type='text'
-                    name="Email"
+                    name="confirmEmail"
                     required
+                    onChange={handleFormInput}
                 />
 
                 <div className="names">
                     <OutlinedInput
                         className='firsname'
                         placeholder="Passoword"
-                        type='text'
-                        name="firtname"
+                        type='password'
+                        name="password"
                         required
+                        onChange={handleFormInput}
                     />
                     <OutlinedInput
                         className='middlename'
                         placeholder="Confirm your Password"
-                        type='text'
-                        name="middlename"
+                        type='password'
+                        name="confirmPassword"
                         required
+                        onChange={handleFormInput}
                     />
                 </div>
                 <br />
                 <InputLabel id="label">City</InputLabel>
-                <Select labelId="label" id="select">
+                <Select
+                    onChange={handleFormInput}
+                    labelId="label"
+                    id="select"
+                    name='city'
+                    required
+                >
                     <MenuItem value="10">Chicago</MenuItem>
                     <MenuItem value="20">NYC</MenuItem>
                 </Select>
@@ -121,30 +209,33 @@ function MedicalProfessional() {
                         <OutlinedInput
                             className='firsname'
                             placeholder="National ID"
-                            type='text'
-                            name="nationalid"
+                            type='number'
+                            name="nationalId"
                             required
+                            onChange={handleFormInput}
                         />
                         <OutlinedInput
                             className='middlename'
                             placeholder="Employee ID"
-                            type='text'
-                            name="employeeid"
+                            type='number'
+                            name="employeeId"
                             required
+                            onChange={handleFormInput}
                         />
                     </div>
                     <br />
+                    <span>Professional Role</span>
                     <div className='professional-role'>
-                        <span>Professional Role</span>
-                    &nbsp;
-                    &nbsp;
-                    <label htmlFor="doctor">
+                        <label htmlFor="doctor">
                             <input
                                 type="radio" name="role"
                                 id="doctor"
+                                name="role"
                                 value="doctor"
+                                required
+                                onChange={handleFormInput}
                             />&nbsp;
-                    <span>Male</span>
+                            <span>Doctor</span>
                         </label>
                         <label htmlFor="nurse">
                             <input
@@ -152,31 +243,44 @@ function MedicalProfessional() {
                                 name="role"
                                 id="nurse"
                                 value="nurse"
+                                required
+                                onChange={handleFormInput}
                             />&nbsp;
-                    <span>Female</span>
+                            <span>Nurse</span>
                         </label>
                     </div>
+
                     <br />
                     <div className="names">
                         <OutlinedInput
                             className='firsname'
                             placeholder="Medical Designation"
                             type='text'
-                            name="designation"
+                            name="medicalDescription"
                             required
+                            onChange={handleFormInput}
                         />
-                        <InputLabel id="label">City</InputLabel>
-                        <Select className='middlename' labelId="label" id="select">
-                            <MenuItem value="10">Chicago</MenuItem>
-                            <MenuItem value="20">NYC</MenuItem>
-                        </Select>
+                        <div>
+                            <InputLabel style={{ marginLeft: '' }} id="label">Hospital</InputLabel>
+                            <Select
+                                style={{ width: '15rem' }}
+                                labelId="label"
+                                id="select"
+                                name="hospital"
+                                required
+                                onChange={handleFormInput}
+                            >
+                                <MenuItem value="10">Chicago</MenuItem>
+                                <MenuItem value="20">NYC</MenuItem>
+                            </Select>
+                        </div>
                     </div>
 
                 </fieldset>
                 <br />
-                <Button 
-                    onClick={handleRegister} 
-                    variant="contained" 
+                <Button
+                    type="submit"
+                    variant="contained"
                     color="primary"
                 >
                     Register
