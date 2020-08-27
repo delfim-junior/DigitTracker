00ee420c8e00ab6ac6960ea5bd2dc10508bd5312
delfim-junior <delfim.uu.junior@gmail.com>
2020-08-27 02:21:02 +0200

    form validation and toasts alert

--- tracker/src/pages/Register/UserTracker.js
+++ tracker/src/pages/Register/UserTracker.js
@@ -5,6 +5,10 @@ import { OutlinedInput } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import validator from 'validator'
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import './styles.css';
 
 
@@ -27,6 +31,23 @@ function UserTracker() {
 
     const [confirmation, setConfirmation] = useState(false)
     const [successMessage, setSuccessMessage] = useState(false)
+    const [formInput, setFormInput] = useState({
+        firstName: '',
+        middleName: '',
+        lastName: '',
+        gender: '',
+        birthday: '',
+        email: '',
+        nationality: '',
+        province: '',
+        city: '',
+        houseNumber: '',
+        streetName: '',
+        idType: '',
+        idNumber: '',
+        phoneNumber: '',
+
+    })
 
     function handleShowConfirmation() {
         setConfirmation(true)
@@ -36,211 +57,267 @@ function UserTracker() {
         history.push('/')
     }
 
-    function handleRegister() {
-        setSuccessMessage(true)
-        setTimeout(() => {
-            history.push('/')
-        }, 3000)
+    function handleFormInput(event) {
+        const { name, value } = event.target
+        setFormInput({ ...formInput, [name]: value })
+    }
+
+    function handleRegister(event) {
+        event.preventDefault()
+        //data ready to use in the backend
+        const {
+            firstName,
+            middleName,
+            lastName,
+            gender,
+            birthday,
+            email,
+            nationality,
+            province,
+            city,
+            houseNumber,
+            streetName,
+            idType,
+            idNumber,
+            phoneNumber
+        } = formInput
+
+        const isEmail = validator.isEmail(email)
+        if (isEmail) {
+            //Backend...
+            setSuccessMessage(true)
+            setTimeout(() => {
+                history.push('/')
+            }, 3000)
+        }
+        else {
+            toast.error('The entered Email is not valid')
+        }
+
     }
 
     return (
-        <form className="user-container">
-            <header>
-                <FiArrowLeft onClick={handleGoBack} size={30} color="#27AE60" />
-                <h6 onClick={handleGoBack}>Home</h6>
-            </header>
-            <div className="icon">
-                <FiUserPlus size={80} />
-            </div>
-            <div className="names">
-                <OutlinedInput
-                    className='firsname'
-                    placeholder="First Name"
-                    type='text'
-                    name="firtname"
-                    required
-                />
-                <OutlinedInput
-                    className='middlename'
-                    placeholder="Middle Name"
-                    type='text'
-                    name="middlename"
-                    required
-                />
-            </div>
-            <OutlinedInput
-                className=''
-                placeholder="Last Name"
-                type='text'
-                name="lastname"
-                required
-            />
-            <div className='gender'>
-                <span>Gender</span>
-                &nbsp;
-                &nbsp;
-                <label htmlFor="male">
-                    <input
-                        type="radio" name="identify"
-                        id="male"
-                        value="user"
-                    />&nbsp;
-                    <span>Male</span>
-                </label>
-                <label htmlFor="female">
-                    <input
-                        type="radio"
-                        name="identify"
-                        id="female"
-                        value="professional"
-                    />&nbsp;
-                    <span>Female</span>
-                </label>
-            </div>
-            <TextField
-                id="date"
-                label="Birthday"
-                type="date"
-                defaultValue="2017-05-24"
-                className={classes.textField}
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-            <OutlinedInput
-                className=''
-                placeholder="Email"
-                type='text'
-                name="Email"
-                required
-            />
-
-            <div className="names">
-                <OutlinedInput
-                    className='firsname'
-                    placeholder="Nationality"
-                    type='text'
-                    name="firtname"
-                    required
-                />
-                <OutlinedInput
-                    className='middlename'
-                    placeholder="Province"
-                    type='text'
-                    name="middlename"
-                    required
-                />
-            </div>
-            <div>
-                <OutlinedInput
-                    className='first-line'
-                    placeholder="City"
-                    type='text'
-                    name="city"
-                    required
-                />
-                <OutlinedInput
-                    className='first-line'
-                    placeholder="House Number"
-                    type='text'
-                    name="housenumber"
-                    required
-                />
-                <OutlinedInput
-                    className=''
-                    placeholder="Street Name"
-                    type='text'
-                    name="streetname"
-                    required
-                />
-            </div>
-            <fieldset className="idinfo">
-                <legend>Addictional Information</legend>
+        <>
+            <ToastContainer />
+            <form onSubmit={handleRegister} className="user-container">
+                <header>
+                    <FiArrowLeft onClick={handleGoBack} size={30} color="#27AE60" />
+                    <h6 onClick={handleGoBack}>Home</h6>
+                </header>
+                <div className="icon">
+                    <FiUserPlus size={80} />
+                </div>
                 <div className="names">
                     <OutlinedInput
                         className='firsname'
-                        placeholder="ID Type"
+                        placeholder="First Name"
                         type='text'
-                        name="firtname"
+                        name="firstName"
                         required
+                        onChange={handleFormInput}
                     />
                     <OutlinedInput
                         className='middlename'
-                        placeholder="ID Number"
+                        placeholder="Middle Name"
                         type='text'
-                        name="middlename"
+                        name="middleName"
                         required
+                        onChange={handleFormInput}
                     />
                 </div>
-                <br />
+                <OutlinedInput
+                    className=''
+                    placeholder="Last Name"
+                    type='text'
+                    name="lastName"
+                    required
+                    onChange={handleFormInput}
+                />
+                <div className='gender'>
+                    <span>Gender</span>
+                &nbsp;
+                &nbsp;
+                <label htmlFor="male">
+                        <input
+                            type="radio" name="identify"
+                            id="male"
+                            name="gender"
+                            value="M"
+                            onChange={handleFormInput}
+                            required
+                        />&nbsp;
+                    <span>Male</span>
+                    </label>
+                    <label htmlFor="female">
+                        <input
+                            type="radio"
+                            name="gender"
+                            id="male"
+                            value="F"
+                            onChange={handleFormInput}
+                            required
+                        />&nbsp;
+                    <span>Female</span>
+                    </label>
+                </div>
+                <TextField
+                    id="date"
+                    label="Birthday"
+                    type="date"
+                    name="birthday"
+                    defaultValue="2017-05-24"
+                    className={classes.textField}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    required
+                    onChange={handleFormInput}
+                />
+                <OutlinedInput
+                    className=''
+                    placeholder="Email"
+                    type='text'
+                    name="email"
+                    required
+                    onChange={handleFormInput}
+                />
+
                 <div className="names">
                     <OutlinedInput
                         className='firsname'
-                        placeholder="Phone Number"
+                        placeholder="Nationality"
                         type='text'
-                        name="firtname"
+                        name="nationality"
                         required
+                        onChange={handleFormInput}
                     />
+                    <OutlinedInput
+                        className='middlename'
+                        placeholder="Province"
+                        type='text'
+                        name="province"
+                        required
+                        onChange={handleFormInput}
+                    />
+                </div>
+                <div>
+                    <OutlinedInput
+                        className='first-line'
+                        placeholder="City"
+                        type='text'
+                        name="city"
+                        required
+                        onChange={handleFormInput}
+                    />
+                    <OutlinedInput
+                        className='first-line'
+                        placeholder="House Number"
+                        type='number'
+                        name="houseNumber"
+                        required
+                        onChange={handleFormInput}
+                    />
+                    <OutlinedInput
+                        className=''
+                        placeholder="Street Name"
+                        type='text'
+                        name="streetName"
+                        required
+                        onChange={handleFormInput}
+                    />
+                </div>
+                <fieldset className="idinfo">
+                    <legend>Addictional Information</legend>
+                    <div className="names">
+                        <OutlinedInput
+                            className='firsname'
+                            placeholder="ID Type"
+                            type='text'
+                            name="idType"
+                            required
+                            onChange={handleFormInput}
+                        />
+                        <OutlinedInput
+                            className='middlename'
+                            placeholder="ID Number"
+                            type='number'
+                            name="idNumber"
+                            required
+                            onChange={handleFormInput}
+                        />
+                    </div>
+                    <br />
+                    <div className="names">
+                        <OutlinedInput
+                            className='firsname'
+                            placeholder="Phone Number"
+                            type='text'
+                            name="phoneNumber"
+                            required
+                            onChange={handleFormInput}
+                        />
+                        <Button
+                            color='primary'
+                            component='button'
+                            variant='contained'
+                            className='middlename'
+                            onClick={handleShowConfirmation}
+                        >
+                            Confirm
+                    </Button>
+                    </div>
+                    <br />
+                    {
+                        confirmation &&
+                        <div className="names">
+                            <h6
+                                className='firsname'
+                            >
+                                Verify the mobile phone number through SMS
+                        </h6>
+                            <OutlinedInput
+                                className='middlename'
+                                placeholder="Confirmation Code"
+                                type='text'
+                                name="middlename"
+                                required
+                            />
+                        </div>
+                    }
+                </fieldset>
+                <div className="maps">
+                    MAPS
+            </div>
+                <Button variant="contained" color="default">
+                    Confirm Location
+            </Button>
+                <div className="names">
+                    <Button
+                        color='secondary'
+                        component='button'
+                        variant='contained'
+                        className='firsname'
+                        onClick={() => history.goBack()}
+                    >
+                        Cancel
+                    </Button>
                     <Button
                         color='primary'
                         component='button'
                         variant='contained'
                         className='middlename'
-                        onClick={handleShowConfirmation}
+                        type='submit'
+
                     >
-                        Confirm
+                        Register
                     </Button>
                 </div>
-                <br />
                 {
-                    confirmation &&
-                    <div className="names">
-                        <h6
-                            className='firsname'
-                        >
-                            Verify the mobile phone number through SMS
-                        </h6>
-                        <OutlinedInput
-                            className='middlename'
-                            placeholder="Confirmation Code"
-                            type='text'
-                            name="middlename"
-                            required
-                        />
-                    </div>
+                    successMessage &&
+                    <Loading />
                 }
-            </fieldset>
-            <div className="maps">
-                MAPS
-            </div>
-            <Button variant="contained" color="default">
-                Confirm Location
-            </Button>
-            <div className="names">
-                <Button
-                    color='secondary'
-                    component='button'
-                    variant='contained'
-                    className='firsname'
-                    onClick={() => history.goBack()}
-                >
-                    Cancel
-                    </Button>
-                <Button
-                    color='primary'
-                    component='button'
-                    variant='contained'
-                    className='middlename'
-                    onClick={handleRegister}
-                >
-                    Register
-                    </Button>
-            </div>
-            {
-                successMessage &&
-                <Loading />
-            }
-        </form>
+            </form>
+        </>
     )
 }
 
