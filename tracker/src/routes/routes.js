1a84ff52719834915939c11c108d315f75097555
delfim-junior <delfim.uu.junior@gmail.com>
2020-08-30 01:54:21 +0200

    routes adjusts

--- tracker/src/routes/routes.js
+++ tracker/src/routes/routes.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Login from '../pages/Login'
 import PasswordRecovery from '../pages/PasswordRecovery'
-import Register from '../pages/Register'
+import Device from '../pages/Register/Device'
+import SystemUser from '../pages/Register/SystemUser'
 import Admin from '../pages/Admin'
+import Forwarder from '../pages/Forwarder'
 
 
 const Routes = () => (
     <BrowserRouter>
-        <Route exact path='/' component={Login} />
+        <Route exact path='/' component={Forwarder} />
+        <Route path='/login' component={Login} />
         <Route path='/forgot-password' component={PasswordRecovery} />
-        <Route path='/register' component={Register} />
+        <Route path='/register'>
+            <Route path='/register/device-register' component={Device}/>
+            <Route path='/register/user-register' component={SystemUser}/>
+        </Route>
         <Route path='/admin' component={Admin} />
     </BrowserRouter>
 )
