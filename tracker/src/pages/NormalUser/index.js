8ed52bdcd035d16ff40a686b1cb3e1f62916f552
delfim-junior <delfim.uu.junior@gmail.com>
2020-09-04 08:41:05 +0200

    spinner while data are being fetched

--- tracker/src/pages/NormalUser/index.js
+++ tracker/src/pages/NormalUser/index.js
@@ -1,26 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom'
 import { FiTablet, FiEdit } from 'react-icons/fi'
-
-import './styles.css';
 import { Button } from '@material-ui/core';
+
+import Loading from '../Spinners'
+
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './styles.css';
 
 function NormalUser() {
 
   const history = useHistory()
 
-  const [device, setDevice] = useState(null)
+  const [device, setDevice] = useState([null])
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     //Fetch device info from Database here
-    setDevice({
-      imei: '12131213',
-      deviceName: 'IPhone 7',
-      status: 'clear'
-    })
+    setTimeout(() => {
+      setDevice({
+        imei: '12131213',
+        deviceName: 'IPhone 7',
+        status: 'clear'
+      })
+
+      setLoading(false)
+    }, 2000)
   }, [])
 
   function handleDeleteDevice(e) {
     e.preventDefault()
+    toast.success('Device successfully removed')
+
     setDevice(null)
   }
 
@@ -30,70 +42,79 @@ function NormalUser() {
 
   return (
     <div className="normalUser-Container">
+      <ToastContainer />
       {
-        device === null
+        loading
           ?
-          <>
-            <h1>You have no Active Devices</h1>
-            <Button
-              color='primary'
-              component='button'
-              variant='contained'
-              type='submit'
-              onClick={handleDeviceRegister}
-
-            >
-              Add New Device &nbsp; <FiEdit />
-            </Button>
-          </>
+          <Loading action="loading" />
           :
           <>
-            <header>
-              <h4>Your Active Device</h4>
-              <FiTablet size={30} />
-            </header>
-            <br />
-            <form onSubmit={handleDeleteDevice}>
-              <div>
-                <label htmlFor="imei">Device IMEI</label>
-                <input
-                  type='text'
-                  name="imei"
-                  value={device.imei}
-                  disabled
-                />
-              </div>
+            {
+              device === null || device.length === 0
+                ?
+                <>
+                  <h1>You have no Active Devices</h1>
+                  <Button
+                    color='primary'
+                    component='button'
+                    variant='contained'
+                    type='submit'
+                    onClick={handleDeviceRegister}
 
-              <div>
-                <label htmlFor="deviceName">Device Name</label>
-                <input
-                  type='text'
-                  name="deviceName"
-                  value={device.deviceName}
-                  disabled
-                />
-              </div>
+                  >
+                    Add New Device &nbsp; <FiEdit />
+                  </Button>
+                </>
+                :
+                <>
+                  <header>
+                    <h4>Your Active Device</h4>
+                    <FiTablet size={30} />
+                  </header>
+                  <br />
+                  <form onSubmit={handleDeleteDevice}>
+                    <div>
+                      <label htmlFor="imei">Device IMEI</label>
+                      <input
+                        type='text'
+                        name="imei"
+                        value={device.imei}
+                        disabled
+                      />
+                    </div>
 
-              <div>
-                <label htmlFor="infection">Infection Status</label>
-                <input
-                  type='text'
-                  name="infection"
-                  value={device.status}
-                  disabled
-                />
-              </div>
-              <br />
-              <Button
-                color='secondary'
-                component='button'
-                variant='contained'
-                type='submit'
+                    <div>
+                      <label htmlFor="deviceName">Device Name</label>
+                      <input
+                        type='text'
+                        name="deviceName"
+                        value={device.deviceName}
+                        disabled
+                      />
+                    </div>
 
-              >
-                Remove Device
+                    <div>
+                      <label htmlFor="infection">Infection Status</label>
+                      <input
+                        type='text'
+                        name="infection"
+                        value={device.status}
+                        disabled
+                      />
+                    </div>
+                    <br />
+                    <Button
+                      color='secondary'
+                      component='button'
+                      variant='contained'
+                      type='submit'
+
+                    >
+                      Remove Device
               </Button>
-            </form>
+                  </form>
+                </>
+            }
           </>
       }
     </div>
